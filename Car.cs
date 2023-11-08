16e4e0669841bc914e78d2fa888e9b829bc157af
qxseb <sebidani139@gmail.com>
2023-11-08 08:09:20 +0200

    first commit

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD_2023
+{
+    public class Car
+    {
+
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+
+        public void fullThrotle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XD_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrotle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.maxSpeed);
+
+            Opel.fullThrotle();
+            Console.ReadKey();
+        }
+    }
+}
